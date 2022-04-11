a5d623be9c1738595ade60ef276f784cd2f89a17
DaniMeschian <daniel.meschian@mentormate.com>
2022-04-11 17:31:13 +0300

    Second commit

--- src/main/java/Basic.java
+++ src/main/java/Basic.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Basic {
+import files.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class Basic {
+
+    public static void main(String[] args) {
+        Payload payload = new Payload();
+
+        RestAssured.baseURI = "http://bb-training.test.devsmm.com/api";
+
+        //Register a new user
+        given().log().all().header("Accept", "application/json")
+                .contentType("application/json")
+                .body(payload.registerNewUser())
+                .when().post("/register")
+                .then()
+                .assertThat().statusCode(201).body("data.user.first_name", equalTo("Filomena"))
+                .header("server", "Apache/2.4.6 (CentOS) PHP/7.2.17");
+
+        //Login with generated credentials
+        String response  = given().log().all().header("Accept", "application/json")
+                .contentType("multipart/form-data")
+                .multiPart("email", payload.getEmail())
+                .multiPart("password", payload.getPassword())
+                .when().post("/auth/login")
+                .then()
+                .assertThat().statusCode(200)
+                .extract().response().asString();
+
+        JsonPath jsonPath = new JsonPath(response); //for parsing json
+        String token = jsonPath.getString("data.access_token");
+
+        //Get specific information
+        given().log().all().headers("Authorization","Bearer " + token,"Accept", "application/json")
+                .when().get("/users/12616")
+                .then()
+                .assertThat().statusCode(200);
+
+        //Logout
+        given().log().all().headers("Authorization","Bearer " + token,"Accept", "application/json")
+                .when().post("/auth/logout")
+                .then()
+                .assertThat().log().all().statusCode(200);
+    }
 }
--- src/main/java/files/Payload.java
+++ src/main/java/files/Payload.java
@@ -1,2 +1,49 @@
-package files;public class payload {
+package files;
+
+import java.util.Random;
+
+public class Payload {
+
+    private final String email = generateEmail();
+    private final String password = generatePassword();
+
+    private int generateRandomIntNumber() {
+        Random ran = new Random();
+        return ran.nextInt();
+    }
+
+    private String generateEmail() {
+        String email;
+        return email = "daniel.meschian+" + generateRandomIntNumber() + "@mentormate.com";
+    }
+
+    private String generatePassword() {
+        String password;
+        return password = "d5QdKpNZDTYF2Y6" + generateRandomIntNumber();
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String registerNewUser() {
+        String currentPass = this.password;
+        return String.format("{\r\n" +
+                "  \"first_name\": \"Filomena\",\r\n" +
+                "  \"last_name\": \"Dare\",\r\n" +
+                "  \"email\": \"%s\",\r\n" +
+                "  \"password\": \"%s\",\r\n" +
+                "  \"password_confirmation\": \"%s\"\r\n" +
+                "}", getEmail(), currentPass, currentPass);
+    }
+    public String login() {
+        return String.format("{\r\n" +
+                "  \"email\": \"%s\",\r\n" +
+                "  \"password\": \"%s\",\r\n" +
+                "}", getEmail(), getPassword());
+    }
 }
